a1406254f342d329126f1e697ff42c1875ed7831
k3b4bb <enriquemartin1402@gmail.com>
2024-07-06 02:30:13 +0200

    Solve exercises 03-06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(a, b, ...c) {
+    array = Array.from(arguments);
+    result = [];
+    for (let i = 0, l = array.length; i < l; i++) {
+        if (Array.isArray(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if ((typeof min != 'number' || isNaN(min)) || (typeof max != 'number' || isNaN(max))) {
+        return 'ERROR'
+    } 
 
+    if (min > max) {
+        [min, max] = [max, min]
+    }
+    let result = 0;
+    for (let i = min; i <= max; i++) {
+        result += i;
+    }
+    if (result < 0) {
+        return 'ERROR';
+    };
+    return result;
 };
 
 // Do not edit below this line
