a507f6ea8575456b75c4cb089fb9d327e87f2561
k3b4bb <enriquemartin1402@gmail.com>
2024-07-09 20:56:39 +0200

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+function fibonacci(n) {
+  // Convert input to number if it's a string
+  n = parseInt(n);
+  
+  // Check for negative input
+  if (n < 0) return "OOPS";
+  
+  // Base cases
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  
+  // Calculate Fibonacci number
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
 
-};
-
-// Do not edit below this line
 module.exports = fibonacci;
