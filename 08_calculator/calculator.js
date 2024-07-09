21be2ceebb2b951b9e69870e6d229be8370bbbd4
k3b4bb <enriquemartin1402@gmail.com>
2024-07-09 19:22:13 +0200

    Solve execise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((num, acc) => num + acc, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((actual, total) => actual * total, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  else {
+    return num * factorial(num - 1)
+  }
 };
 
 // Do not edit below this line
