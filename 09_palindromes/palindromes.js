8d0797a7a35fb050ced981d3dcedb356384be8fb
k3b4bb <enriquemartin1402@gmail.com>
2024-07-09 19:25:03 +0200

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+  return cleanStr === cleanStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
